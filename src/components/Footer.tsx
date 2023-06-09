be270e129b0f7dd9148015887c1ce56aebd6f4ae
Jaeseo Bae <jae912@uab.edu>
2023-06-09 10:45:25 -0500

    final edits

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ import { Box, Text } from "@chakra-ui/react";
 
 const Footer = () => {
   return (
-    <Box textAlign="center" p={4}>
-      <Text>&copy; Drew Bae. All rights reserved</Text>
+    <Box textAlign="center" p={4} bg={"#0b132bff"}>
+      <Text color={"white"}>&copy; Drew Bae. All rights reserved</Text>
     </Box>
   );
 };
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -32,18 +32,28 @@ const ProjectCard = ({
     <Card maxWidth={maxWidth} mx="auto" overflow="hidden">
       <Image src={picture} />
       <CardBody p={4} bg="gray.700">
-        <Heading fontSize="1.25rem" mb={2}>
+        <Heading fontSize="1.25rem" mb={2} color={"white"}>
           {heading}
         </Heading>
-        <Text>{children}</Text>
+        <Text color={"white"}>{children}</Text>
         <Link href={link} target="_blank" rel="noopener noreferrer">
-          <Button as="span" mt={3} color="#5bc0beff">
+          <Button
+            bg="rgba(128, 128, 128, 0.3)"
+            as="span"
+            mt={3}
+            color="#5bc0beff"
+          >
             Link
           </Button>
         </Link>
         <HStack mt={5} wrap="wrap" overflowX="auto">
           {badges.map((badge, index) => (
-            <Badge colorScheme="blue" key={index} whiteSpace="nowrap">
+            <Badge
+              colorScheme="blue"
+              color="blue.500"
+              key={index}
+              whiteSpace="nowrap"
+            >
               {badge}
             </Badge>
           ))}
